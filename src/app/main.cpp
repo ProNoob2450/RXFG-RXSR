0f4e344ff3ee29d63d37553bb84e7fd840e773ce
ProNoob2450 <121444947+ProNoob2450@users.noreply.github.com>
2026-06-07 07:21:52 +0530

    Bootstrap Qt QML application

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,25 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
 #include "logging/Logger.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
-    rxfg::Logger::Initialize();
-    rxfg::Logger::Info("RXFG-RXSR starting up");
+    QGuiApplication app(argc, argv);
 
-    return 0;
+    rxfg::Logger::Initialize();
+    rxfg::Logger::Info("RXFG-RXSR startup");
+
+    QQmlApplicationEngine engine;
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        [](){ QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+
+    engine.loadFromModule("RXFG", "Main");
+
+    return app.exec();
 }
